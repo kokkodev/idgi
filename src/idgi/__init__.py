eedfc1d28b6661a48c9163a7df333c555e4ec6d0
kokkodev <kokkodev@users.noreply.github.com>
2025-08-12 09:51:12 +0800

    Bump version to 0.1.1 and simplify pre-commit config

--- src/idgi/__init__.py
+++ src/idgi/__init__.py
@@ -2,6 +2,6 @@
 idgi - A command-line tool for exploring and visualizing large Python codebases.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "idgi developers"
 __email__ = "developers@idgi.dev"
